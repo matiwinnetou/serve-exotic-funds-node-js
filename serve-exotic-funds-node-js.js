f353ac40f72f50d9e3c021a3c05be8cc1eeab16e
Mateusz Czeladka <mateusz.szczap@gmail.com>
2018-09-23 23:55:45 +0200

    serve-exotic-funds-node-js

---
+++ serve-exotic-funds-node-js.js
@@ -0,0 +1,54 @@
+const cheerio = require('cheerio');
+const request = require('request');
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port);
+
+server.timeout = 1000 * 60 * 10; // 10 minutes
+
+// Use middleware to set the default Content-Type
+app.use(function (req, res, next) {
+    res.header('Content-Type', 'application/json');
+    next();
+});
+
+var fundsPrices = {};
+
+app.get('/', (req, res) => {
+    request('http://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F000002QT3', (err, schroderRes, schroderBody) => {
+        if (err) { return console.log(err); }
+        const $ = cheerio.load(schroderBody);
+        const selector = "#overviewQuickstatsDiv > table > tbody > tr:nth-child(2) > td.line.text";
+        const schroderPriceInPences = $(selector).text().replace("GBX","").trim();
+        const schroderPriceInPounds = schroderPriceInPences / 100;
+
+
+        request('http://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F0GBR06XQ5', (err, goldRes, goldBody) => {
+            if (err) { return console.log(err); }
+            const $ = cheerio.load(goldBody);
+            const selector = "#overviewQuickstatsDiv > table > tbody > tr:nth-child(2) > td.line.text";
+            const goldPriceInPences = $(selector).text().replace("GBX","").trim();
+            const goldPriceInPounds = goldPriceInPences / 100;
+    
+            fundsPrices.Inclusive_Class_H_Accumulation_GBP = schroderPriceInPounds;
+            fundsPrices.Smith_and_Williamson_Global_Gold_and_Resources_Inclusive_Class_A_Income_GBP = goldPriceInPounds;
+
+            res.send(JSON.stringify(fundsPrices));
+        });
+
+
+        
+    });
+
+
+    //http://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F0GBR06XQ5
+
+})
+
+
+
+
