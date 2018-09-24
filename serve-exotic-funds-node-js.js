9636d3ffe90faff08b6454fb2bebfec068adfa34
Mateusz Czeladka <mateusz.szczap@gmail.com>
2018-09-24 23:43:17 +0200

    Update serve-exotic-funds-node-js.js

--- serve-exotic-funds-node-js.js
+++ serve-exotic-funds-node-js.js
@@ -16,24 +16,23 @@ app.use(function (req, res, next) {
     next();
 });
 
-var fundsPrices = {};
-
 app.get('/', (req, res) => {
-    request('http://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F000002QT3', (err, schroderRes, schroderBody) => {
+    request('http://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F00000T21Q', (err, schroderRes, schroderBody) => {
         if (err) { return console.log(err); }
         const $ = cheerio.load(schroderBody);
         const selector = "#overviewQuickstatsDiv > table > tbody > tr:nth-child(2) > td.line.text";
-        const schroderPriceInPences = $(selector).text().replace("GBX","").trim();
-        const schroderPriceInPounds = schroderPriceInPences / 100;
+        const schroderPriceInPounds = parseFloat($(selector).text().replace("GBX","").trim());
 
         request('http://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id=F0GBR06XQ5', (err, goldRes, goldBody) => {
             if (err) { return console.log(err); }
             const $ = cheerio.load(goldBody);
             const selector = "#overviewQuickstatsDiv > table > tbody > tr:nth-child(2) > td.line.text";
             const goldPriceInPences = $(selector).text().replace("GBX","").trim();
-            const goldPriceInPounds = goldPriceInPences / 100;
-    
-            fundsPrices.Schroder_Managed_Balanced_Inclusive_Class_H_Accumulation_GBP = schroderPriceInPounds;
+            const goldPriceInPounds = parseFloat(goldPriceInPences / 100);
+                
+            const fundsPrices = {};
+
+            fundsPrices.Schroder_Tokyo_Fund_H_Accumulation_GBP = schroderPriceInPounds;
             fundsPrices.Smith_and_Williamson_Global_Gold_and_Resources_Inclusive_Class_A_Income_GBP = goldPriceInPounds;
 
             res.send(JSON.stringify(fundsPrices));
